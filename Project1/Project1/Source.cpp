955e6f0e9f2cc8a69459f71062b8e65c4d74e64a
MaksymchukMaksym <68642705+MaksymchukMaksym@users.noreply.github.com>
2020-08-05 12:45:23 +0300

    1

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
-
+#include <conio.h>
 using namespace std;
-
-int main() {
-	//йобанний в рот
+class Alpha {
+public:
+	static int n;
+	Alpha();
+	~Alpha();
+};
+int Alpha::n = 0;
+Alpha::Alpha()
+{
+		if (n == 0) cout << "First object has been created" << endl;
+	n++;
+}
+Alpha::~Alpha()
+{
+	n--;
+	if (n == 0) cout << "Last element has been deleted";
+}
+int main()
+{
+	Alpha* a;
+	a = new Alpha[5];
+	delete[] a;
 	return 0;
 }
