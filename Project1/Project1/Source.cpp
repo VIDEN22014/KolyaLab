9f556599f1bbe126315bd403fc2aa6a2312ce325
VIDEN22014 <dxbogdan4@gmail.com>
2020-08-05 12:37:34 +0300

    added 3

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -2,7 +2,40 @@
 
 using namespace std;
 
-int main() {
+class String {
+public:
+	char* p=nullptr;
+	int size=0;
+	String() {}
+	String(char *string) {
+		p = new char[strlen(string)];
+		size = strlen(string);
+		for (int i = 0; i <= size; i++)
+		{
+			*(p + i) = *(string + i);
+		}
+	}
+	String(char string) {
+		p = new char;
+		size = 1;
+		*p = string;
+		*(p + 1) = '\0';
+	}
+	int putSize() { return size; }
+	void clearSize() { size = 0;}
+	void clear() {
+	p = new char;
+	*p = '\0';
+	}
 
+};
+
+int main() {
+	char ch;
+	cin >> ch;
+	String a(ch);
+	cout << a.p<<endl;
+	a.clear();
+	cout << a.p<<endl;
 	return 0;
 }
