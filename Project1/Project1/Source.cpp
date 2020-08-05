95d3a78fc1cf8cdfff82b8a2ae2860d1e779f5e4
VIDEN22014 <dxbogdan4@gmail.com>
2020-08-05 14:13:29 +0300

    Added 3 Lab

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -3,11 +3,14 @@
 using namespace std;
 
 class String {
+protected:
+	char* p;
+	int size;
 public:
-	char* p=nullptr;
-	int size=0;
-	String() {}
-	String(char *string) {
+	String() {
+		clear();
+	}
+	String(char* string) {
 		p = new char[strlen(string)];
 		size = strlen(string);
 		for (int i = 0; i <= size; i++)
@@ -21,21 +24,91 @@ public:
 		*p = string;
 		*(p + 1) = '\0';
 	}
-	int putSize() { return size; }
-	void clearSize() { size = 0;}
-	void clear() {
-	p = new char;
-	*p = '\0';
+	String(const String& obj) {
+		delete p;
+		size = strlen(obj.p);
+		p = new char[size];
+		for (int i = 0; i <= size; i++)
+		{
+			*(p + i) = *(obj.p + i);
+		}
 	}
+	~String() {}
 
+	int putSize() { return size; }
+	void clearSize() { size = 0; }
+	void clear() {
+		p = new char;
+		*p = '\0';
+		size = 0;
+	}
+	void Display() { cout << p << endl; }
+};
+
+class StringId :public String {
+public:
+	StringId() { clear(); }
+	StringId(char* string) :String(string) {
+		if (isNotID())clear();
+	}
+	StringId(char string) :String(string) {
+		if (isNotID())clear();
+	}
+	StringId(StringId& obj) {
+		if (isNotID()) { clear(); }
+		else {
+			delete p;
+			size = strlen(obj.p);
+			p = new char[size];
+			for (int i = 0; i <= size; i++)
+			{
+				*(p + i) = *(obj.p + i);
+			}
+		}
+	}
+	~StringId() {}
+	void Uppercase() {
+		for (int i = 0; i < size; i++)
+		{
+			if (*(p + i) >= 97 && *(p + i) <= 122) *(p + i) -= 32;
+		}
+
+	}
+	void Lowercase() {
+		for (int i = 0; i < size; i++)
+		{
+			if (*(p + i) >= 65 && *(p + i) <= 90) *(p + i) += 32;
+		}
+	}
+	char* find(char a) {
+		for (int i = 0; i < size; i++)
+		{
+			if (*p == a)return (p + i);
+		}
+		return nullptr;
+	}
+	bool isNotID() {
+		for (int i = 1; i < size; i++)
+		{
+			if (*p >= 48 && *p <= 57) { return true; }
+			if (*(p + i) < 48 || *(p + i) > 122) { return true; }
+			if (*(p + i) > 57 && *(p + i) < 65) { return true; }
+			if (*(p + i) > 90 && *(p + i) < 97 && *(p + i) != 95) { return true; }
+		}
+		return false;
+	}
 };
 
 int main() {
-	char ch;
+	char ch[100];
 	cin >> ch;
-	String a(ch);
-	cout << a.p<<endl;
-	a.clear();
-	cout << a.p<<endl;
+	StringId a(ch);
+	StringId b(a);
+	StringId c;
+	b.Lowercase();
+	b.Display();
+	b.Uppercase();
+	b.Display();
+	c.Display();
 	return 0;
 }
