3c9deb6906a0fc041575eb4be1c86feeb929d887
VIDEN22014 <dxbogdan4@gmail.com>
2020-08-05 16:10:43 +0300

    Added lab 2

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,54 +1,113 @@
 #include <iostream>
-#include "univer.h"
+#include <Windows.h>
+#include <string>
+#include <vector>
+
 using namespace std;
-int count_facultet;
+
+class University {
+public:
+	class Faculty {
+	public:
+		struct Data
+		{
+			string faculty_name;
+			string decan_prizv;
+			int group_count;
+			vector<int> group;
+
+		}data;
+
+		Faculty* next;
+		Faculty(Data a, Faculty* p = nullptr) { data = a; next = p; };
+
+		void add_group(int a) {
+			this->data.group.push_back(a);
+			this->data.group_count++;
+		}
+		void del_group() {
+			this->data.group.pop_back();
+			this->data.group_count--;
+		}
+		void calc_stud() {
+			int stud_count = 0;
+			for (int i = 0; i < data.group_count; i++)
+			{
+				stud_count += data.group[i];
+			}
+			cout<< stud_count << " Students on this faculty"<<endl;
+		}
+
+	};
+	Faculty* head;
+
+	University() { head = nullptr; }
+	~University() {}
+	Faculty* operator[](int a) {
+		int count = 0;
+		Faculty* temp = head;
+		while (temp != nullptr) {
+			if (count == a) { return temp; }
+			temp=temp->next;
+			count++;
+		}
+	}
+	void add_faculty(Faculty::Data a) {
+		if (head == nullptr)
+		{
+			head = new Faculty(a);
+		}
+		else {
+			Faculty* temp = this->head;
+			while (temp->next != nullptr)
+			{
+				temp = temp->next;
+			}
+			temp->next = new Faculty(a);
+		}
+	}
+	void del_faculty() {
+		Faculty* temp = head;
+		while (temp != nullptr)
+		{
+			if (temp->next == nullptr) {
+				head = nullptr;
+
+			}
+			else if (temp->next->next == nullptr) {
+				temp->next = nullptr;
+			}
+			temp = temp->next;
+		}
+	}
+};
+
 int main()
 {
-    int menu;
-    cout << "Enter count facultet : ";
-    cin >> count_facultet;
-    univer* array = new univer[count_facultet];
-    do {
-        cout << "1.Input" << endl;
-        cout << "2.Show" << endl;
-        cout << "3.Add group" << endl;
-        cout << "4.Delete group" << endl;
-        cout << "5.Calc group" << endl;
-        cin >> menu;
-        switch (menu) {
-        case 1:
-            for (int i = 0; i < count_facultet; i++) {
-                cout << "===============================================" << endl;
-                array[i].input();
-                cout << "===============================================" << endl;
-            }
-            break;
-        case 2:
-            for (int i = 0; i < count_facultet; i++) {
-                cout << "===============================================" << endl;
-                array[i].view();
-                cout << "===============================================" << endl;
-            }
-            break;
-        case 3:
-            cout << "Faculty of what to add(number) : ";
-            int number_fac;
-            cin >> number_fac;
-            for (int i = number_fac; i == number_fac; i++)
-                array[i].add_group();
-            break;
-        case 4:
-            cout << "Faculty of what to delete(number) : ";
-            int number_fac1;
-            cin >> number_fac1;
-            for (int i = number_fac1; i == number_fac1; i++)
-                array[i].del_group();
-            break;
-        case 5:
-            for (int i = 0; i < count_facultet; i++)
-                array[i].calc_stud();
-            break;
-        }
-    } while (menu < 6);
-    return 0;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	University::Faculty::Data b;
+	b.decan_prizv = "Pilipiv";
+	b.faculty_name = "MIF";
+	b.group_count = 2;
+	b.group.push_back(40);
+	b.group.push_back(15);
+
+	University a;
+	a.add_faculty(b);
+	b.faculty_name = "FIM";
+	b.group[0] = 0;
+	a.add_faculty(b);
+	
+
+	a[0]->calc_stud();
+	a[0]->del_group();
+	a[0]->calc_stud();
+	a[0]->add_group(32);
+	a[0]->calc_stud();
+	a[1]->calc_stud();
+
+	
+	return 0;
 }
